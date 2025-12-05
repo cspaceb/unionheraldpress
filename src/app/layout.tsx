727cff91035aa71b6fa893895dd36185daa1b3db
unknown <cspaceb@gmail.com>
2025-12-05 02:47:59 -0600

    Remove static layout metadata to allow dynamic OG metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -12,11 +12,6 @@ const geistMono = Geist_Mono({
   subsets: ["latin"],
 });
 
-export const metadata: Metadata = {
-  title: "Union Herald Press ***",
-  description: "News that everyone can get into.",
-};
-
 export default function RootLayout({
   children,
 }: Readonly<{
